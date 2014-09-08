0b8c4437155bee7b54285ef35132fd5ae4c7204e
DaikiNakajima3564 <hitanykey.del@gmail.com>
2014-09-08 12:58:13 +0900

    20140908_スクリプト更新_クリック押下し続けて攻撃を続けるように更新

--- Assets/Script/InputManager.cs
+++ Assets/Script/InputManager.cs
@@ -6,6 +6,8 @@ public class InputManager : MonoBehaviour {
 	Vector2 prevPosition;
 	Vector2 delta = Vector2.zero;
 	bool moved = false;
+	bool cMoved = false;
+
 	
 	void Update () {
 	if (Input.GetButtonDown ("Fire1"))
@@ -14,7 +16,7 @@ public class InputManager : MonoBehaviour {
 		if(Input.GetButton("Fire1")){
 			if(Vector2.Distance(slideStartPosition, GetCursorPosition())
 				 >= (Screen.width * 0.1f))
-				moved = true;
+			moved = true;
 		}
 
 		if (!Input.GetButtonUp ("Fire1") && !Input.GetButton ("Fire1"))
@@ -26,11 +28,11 @@ public class InputManager : MonoBehaviour {
 						delta = Vector2.zero;
 
 		prevPosition = GetCursorPosition();
-
+		
 	}
 	
 	public bool Clicked(){
-		if (!moved && Input.GetButtonUp ("Fire1"))
+		if (!moved && Input.GetButtonUp ("Fire1") || Input.GetButton("Fire1"))
 						return true;
 				else
 						return false;
