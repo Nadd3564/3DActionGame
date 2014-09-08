35dbb48ef2b502dead81279a41120ae46df05ce5
DaikiNakajima3564 <hitanykey.del@gmail.com>
2014-09-08 11:23:30 +0900

    20140908_スクリプト追加_エネミーがプレイヤーを見つけると近寄ってくるように更新

---
+++ Assets/Script/SearchArea.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SearchArea : MonoBehaviour {
+	EnemyStatusController enemyCtrl;
+
+	void Start () {
+	//EnemyStatusControllerをキャッシュする
+		enemyCtrl = transform.root.GetComponent<EnemyStatusController> ();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay (Collider other) {
+	//Playerタグをターゲットにする
+		if (other.tag == "Player")
+						enemyCtrl.SetAttackTarget (other.transform);
+	}
+}
