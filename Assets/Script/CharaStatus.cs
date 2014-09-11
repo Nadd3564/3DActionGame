d03ee0f6659503b4d5b69b2d982427b480899296
DaikiNakajima3564 <hitanykey.del@gmail.com>
2014-09-11 17:23:56 +0900

    20140911_ゲームオブジェクト・スクリプト追加_強化アイテム取得時のパワーアップエフェクトを追加

--- Assets/Script/CharaStatus.cs
+++ Assets/Script/CharaStatus.cs
@@ -11,12 +11,14 @@ public class CharaStatus : MonoBehaviour {
 	public bool died = false;
 	public bool powerBoost = false;
 	float powerBoostTime = 0.0f;
+	ParticleSystem powerUpEffect;
 
 	//アイテム取得
 	public void GetItem(DropItem.ItemKind itemKind){
 		switch(itemKind){
 		case DropItem.ItemKind.Attack:
 			powerBoostTime = 10.0f;
+			powerUpEffect.Play();
 			break;
 		case DropItem.ItemKind.Heal:
 			HP = Mathf.Min (HP + MaxHP / 2, MaxHP);
@@ -24,11 +26,23 @@ public class CharaStatus : MonoBehaviour {
 		}
 	}
 
+	void Start(){
+		if(gameObject.tag == "Player"){
+			powerUpEffect = transform.Find("PowerUpEffect").GetComponent<ParticleSystem>();
+		}
+	}
+
 	void Update(){
+		if(gameObject.tag != "Player"){
+			return;
+		}
+
 		powerBoost = false;
-		if(powerBoostTime > 0.0f){
-			powerBoost = true;
-			powerBoostTime = Mathf.Max(powerBoostTime - Time.deltaTime, 0.0f);
+		if (powerBoostTime > 0.0f) {
+						powerBoost = true;
+						powerBoostTime = Mathf.Max (powerBoostTime - Time.deltaTime, 0.0f);
+				} else {
+			powerUpEffect.Stop();		
 		}
 	}
 }
