5b783f96b4af171f007694bb27e8817335507c78
DaikiNakajima3564 <hitanykey.del@gmail.com>
2014-10-02 20:45:33 +0900

    20141002_単体テスト実装@GameRuleSettings

---
+++ Assets/Test/Editor/GameRuleSettingsTest.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using NSubstitute;
+
+namespace Cradle.Test
+{
+	[TestFixture]
+	[Category ("GameRuleSettings Test")]
+	public class GameRuleSettingsTest
+	{
+		public IRuleController iRule;
+		public GameRuleSettingsController gRSController;
+		
+		[SetUp] public void Init()
+		{ 
+			iRule = GetRuleMock ();
+			gRSController = GetControllerMock (iRule);	
+		}
+		
+		[TearDown] public void Cleanup()
+		{
+			
+		}
+		
+		//正常値テスト
+		[Test]
+		[Category ("Calc Test")]
+		public void CalcTimeTest ()
+		{
+			Assert.That (gRSController.CalcTime(), Is.EqualTo (0.0f));
+		}
+		
+		[Test]
+		[Category ("Calc Test")]
+		[TestCase(1)]
+		[TestCase(true)]
+		[TestCase(false)]
+		[TestCase(null)]
+		[TestCase("string")]
+		public void GameFlgsTest(bool flg) 
+		{
+			gRSController.SetGameClear (true);
+			gRSController.SetGameOver (false);
+			Assert.That (gRSController.GameFlgs(), Is.EqualTo(flg));		
+		}
+
+		[Test]
+		[Category ("Calc Test")]
+		[TestCase(1)]
+		[TestCase(true)]
+		[TestCase(false)]
+		[TestCase(null)]
+		[TestCase("string")]
+		public void TimeRemainingTest(bool flg) 
+		{
+			gRSController.SetCountSceneChangeTime (10.0f);
+			Assert.That (gRSController.TimeRemaining(), Is.EqualTo(flg));		
+		}
+		
+
+		private IRuleController GetRuleMock () {
+			return Substitute.For<IRuleController> ();
+		}
+		
+		private GameRuleSettingsController GetControllerMock(IRuleController iRule) {
+			var gRSController = Substitute.For<GameRuleSettingsController> ();
+			gRSController.SetRuleController (iRule);
+			gRSController.CalcTime ().Returns (0.0f);
+			return gRSController;
+		}
+		
+	}
+}
+
