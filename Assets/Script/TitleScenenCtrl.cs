3e7e4416bf827ded97f524fe8ddc6f28dcd6fd34
DaikiNakajima3564 <hitanykey.del@gmail.com>
2014-10-24 19:07:04 +0900

    スクリプト_タイトル画面更新 ・Enterを押すとID入力へフォーカス、ID項目に文字がある場合に再度Enterを押すとPassword入力へフォーカスするよう更新

--- Assets/Script/TitleScenenCtrl.cs
+++ Assets/Script/TitleScenenCtrl.cs
@@ -9,44 +9,54 @@ public class TitleScenenCtrl : MonoBehaviour {
 	    // タイトル画面テクスチャ
 	    public Texture2D bgTexture;
 		public string ID = "";
-		public string passwordToEdit = "user";
+		public string passwordToEdit = "";
 		private  LogIn.SpaceBook logIn;
+
 
 		void OnGUI()
 	    {
-			//入力IDが一致する場合、次のシーンへ
-			if(Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return && CheckID(ID)){
-				if( GUI.GetNameOfFocusedControl() == "GETID" && CheckPass(passwordToEdit)){
-					Application.LoadLevel("PlayScene");
-				}
-			}
+			// スタイルを準備.
+			GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
 
-	        // スタイルを準備.
-	        GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
-
-			
 			// 解像度対応
 	        GUI.matrix = Matrix4x4.TRS(
 	            Vector3.zero,
 	            Quaternion.identity,
 	            new Vector3(Screen.width / 854.0f, Screen.height / 480.0f, 1.0f));
+
 	        // タイトル画面テクスチャ表示
 	        GUI.DrawTexture(new Rect(0.0f, 0.0f, 854.0f, 480.0f), bgTexture);
 
+			//入力ユーザ情報が一致する場合、次のシーンへ
+			if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return && CheckID (ID) || GUI.Button (new Rect (327, 330, 100, 20), "LogIn", buttonStyle)) {
+					if (GUI.GetNameOfFocusedControl () == "MyPassField" && CheckPass (passwordToEdit)) {
+						Application.LoadLevel ("PlayScene");
+					}
+				}
+
+			//ゲーム終了(Exitボタンを押す、または、Escapeを押す)
+			if (GUI.Button (new Rect (427, 330, 100, 20), "Exit", buttonStyle) || Input.GetKeyDown(KeyCode.Escape))
+				Application.Quit();
+
 			//ユーザIDとパスワード入力フィールド表示
+			GUI.SetNextControlName("MyIdField");
 			ID = GUI.TextField(new Rect(327,290,200,20),ID);
-			GUI.SetNextControlName ("GETID");
+			GUI.SetNextControlName ("MyPassField");
 			passwordToEdit = GUI.PasswordField(new Rect(327, 310, 200, 20), passwordToEdit, "*"[0], 25);
-		
+
+			//Enterを押すとID項目へフォーカス
+			if (Event.current.type == EventType.KeyUp && Event.current.keyCode == KeyCode.Return)
+				GUI.FocusControl ("MyIdField");
+
+			//Enterを押すとPassword項目へフォーカス(ID項目に文字が存在する場合)
+			if(Event.current.type == EventType.KeyUp && Event.current.keyCode == KeyCode.Return && ID != "")
+				GUI.FocusControl ("MyPassField");
+
+
 		}
+
 
 		//ユーザ認証
-		private bool Authentication(string ID, string Password){
-			if(CheckID(ID) && CheckPass(Password))
-				return true;
-			return false;
-		}
-
 		private bool CheckID(string ID){
 			if (ID != "user") {
 				Debug.Log("ID : Failed");
