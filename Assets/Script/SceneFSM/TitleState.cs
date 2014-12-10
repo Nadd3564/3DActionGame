1c750584d408a6d58968672512d3a49b76c1cc93
DaikiNakajima3564 <hitanykey.del@gmail.com>
2014-12-10 12:49:42 +0900

    単体テスト_テストのため、リファクター@TitleState

--- Assets/Script/SceneFSM/TitleState.cs
+++ Assets/Script/SceneFSM/TitleState.cs
@@ -10,43 +10,58 @@ namespace Cradle.DesignPattern {
 			//初期化
 			manager = stateManager;
 			Time.timeScale = 0;
-			Debug.Log("change");
 		}
 
 
 		public void StateUpdate() {
-			if(Input.GetKeyUp(KeyCode.Return)) { 
-				Application.LoadLevel("LogInScene");
-				Time.timeScale = 0;
-				manager.SwitchState(new LogInState(manager)); 
-			}
+			InputWithSwitchState (GetKeyUpRet());
 		}
 		
 		// Startボタンを生成して、ボタンが押されたときに、LogInStateに遷移
 		public void Render() {
-			// スタイルを準備.
-			GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
-			
+
 			// 解像度対応
-			GUI.matrix = Matrix4x4.TRS(
-				Vector3.zero,
-				Quaternion.identity,
-				new Vector3(Screen.width / 854.0f, Screen.height / 480.0f, 1.0f));
+			ResolutionSupport ();
 			
 			// タイトル画面テクスチャ表示
 			GUI.DrawTexture(new Rect(0.0f, 0.0f, 854.0f, 480.0f), manager.GetBgTexture());
 			
-			//スタートボタン
-			if(GUI.Button(new Rect(327, 290, 200, 54), "Start", buttonStyle)){
+			//スタートボタン描画とクリック時に遷移
+			InputWithSwitchState(StartButton());
+		}
+
+
+		public bool GetKeyUpRet(){
+			return Input.GetKeyUp (KeyCode.Return);
+		}
+
+		public bool StartButton(){
+			return GUI.Button(new Rect(327, 290, 200, 54), "Start", ButtonStyle());
+		}
+
+		//ログインシーンへ遷移
+		public void InputWithSwitchState(bool method){
+			if(method) { 
 				Application.LoadLevel("LogInScene");
 				Time.timeScale = 0;
 				manager.SwitchState(new LogInState(manager)); 
 			}
 		}
 
-		public void StateFixedUpdate(){
-
+		// ボタンのスタイルを準備.
+		public GUIStyle ButtonStyle(){
+			GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+			return buttonStyle;
 		}
+
+		//解像度
+		void ResolutionSupport(){
+			GUI.matrix = Matrix4x4.TRS(
+				Vector3.zero,
+				Quaternion.identity,
+				new Vector3(Screen.width / 854.0f, Screen.height / 480.0f, 1.0f));	
+		}
+
 
 	}
 	
