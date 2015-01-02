49d1462329badae619ba3360d947547c47c6eb5d
DaikiNakajima3564 <hitanykey.del@gmail.com>
2015-01-02 16:04:25 +0900

    単体テスト_テスト追加@Authentication ・フィールド正常値テスト追加

---
+++ Assets/Test/Editor/AuthenticationTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Cradle.DesignPattern;
+using NSubstitute;
+
+
+namespace Cradle.Test
+{
+	[TestFixture]
+	[Category ("AuthenticationTest Test")]
+	public class AuthenticationTest
+	{
+		private Authentication aut;
+		private SceneManagerController manager;
+		
+		[SetUp] public void Init()
+		{
+			aut = Substitute.For<Authentication>();
+		}
+		
+		[TearDown] public void Cleanup()
+		{
+			
+		}
+		
+
+		//正常値テスト（オブジェクト）
+		[Test]
+		[Category ("id Test")]
+		public void idTest () 
+		{
+			Assert.Null (aut.GetId());
+		}
+
+
+		//正常値テスト（メソッド）
+		[Test]
+		[Category ("SetId Test")]
+		public void SetIdTest () 
+		{
+			string s = "user";
+			aut.SetId ("user");
+			StringAssert.Contains (s, aut.GetId());
+		}
+
+	}
+}
