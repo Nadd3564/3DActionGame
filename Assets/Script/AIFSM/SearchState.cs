5822d46cb7acb594681ee4d10a80d326905802ee
DaikiNakajima3564 <hitanykey.del@gmail.com>
2015-03-26 16:08:34 +0900

    表記を更新@SearchState

--- Assets/Script/AIFSM/SearchState.cs
+++ Assets/Script/AIFSM/SearchState.cs
@@ -7,14 +7,14 @@ namespace Cradle.FM{
 
 		public SearchState(Transform[] wp)
 		{
-			SetWayPoints (wp);
-			SetStateID(FSMStateID.Searching);
+			this.SetWayPoints (wp);
+			this.SetStateID(FSMStateID.Searching);
 		}
 
 
 		public override void Reason(Transform player, Transform npc)
 		{
-			if(LessThanCheckReach(Vector3.Distance(npc.position, player.position), 7.0f))
+			if(this.LessThanCheckReach(Vector3.Distance(npc.position, player.position), 7.0f))
 			{
 				Debug.Log("Switch to Approach State");
 				npc.GetComponent<EnemyCtrl>().SetTransition(Transition.SawPlayer);
@@ -27,17 +27,17 @@ namespace Cradle.FM{
 			npc.GetComponent<EnemyCtrl>().Walking(destPos);
 			
 			//ターゲット地点が遠すぎる場合、パトロール地点を再度設定
-			if(GreaterThanCheckReach(Vector3.Distance(npc.position, destPos), 50.0f))
+			if(this.GreaterThanCheckReach(Vector3.Distance(npc.position, destPos), 50.0f))
 			{
 				Debug.Log("Reached to the destination point/ncalculating the next point");
-				FindNextPoint();
+				this.FindNextPoint();
 			}
 			
 			//ターゲット地点に到着した場合に、パトロール地点を再度設定
-			if(LessThanCheckReach(Vector3.Distance(npc.position, destPos), 0.6f))
+			if(this.LessThanCheckReach(Vector3.Distance(npc.position, destPos), 0.6f))
 			{
 				Debug.Log("Reached to the destination point/ncalculating the next point");
-				FindNextPoint();
+				this.FindNextPoint();
 			}
 		}
 
