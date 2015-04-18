85a869e3a57fd62ac42a929fd9a04b6149a51955
DaikiNakajima3564 <hitanykey.del@gmail.com>
2015-04-19 00:10:57 +0900

    整理

--- Assets/Test/Editor/DropItemtest.cs
+++ Assets/Test/Editor/DropItemtest.cs
@@ -5,137 +5,138 @@ using Cradle.FM;
 
 namespace Cradle.Test
 {
-	[TestFixture]
-	[Category ("DropItem Test")]
-	public class DropItemTest
-	{
-		public IDropItemController iDrop;
-		public DropItemController dIController;
-		public DropItemController.ItemKind kind;
-	
-		[SetUp] public void Init()
-		{ 
-			iDrop = GetItemMock ();
-			iDrop.SetTrigger(true).Returns(false);
-			dIController = GetControllerMock (iDrop);
-		}
-		
-		[TearDown] public void Cleanup()
-		{
-			
-		}
+    [TestFixture]
+    [Category("DropItem Test")]
+    public class DropItemTest
+    {
+        public IDropItemController iDrop;
+        public DropItemController dIController;
+        public DropItemController.ItemKind kind;
+
+        [SetUp]
+        public void Init()
+        {
+            iDrop = GetItemMock();
+            dIController = GetControllerMock(iDrop);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+
+        }
 
 
-		//データ型テスト(オブジェクト)
-		[Test]
-		[Category ("ItemKind Attack Type Test")]
-		public void ItemKindAttackTypeTest() 
-		{
-			Assert.That (dIController.GetAttackKind(), Is.TypeOf(typeof(DropItemController.ItemKind)));
-		}
+        //データ型テスト(オブジェクト)
+        [Test]
+        [Category("ItemKind Attack Type Test")]
+        public void ItemKindAttackTypeTest()
+        {
+            Assert.That(dIController.GetAttackKind(), Is.TypeOf(typeof(DropItemController.ItemKind)));
+        }
 
-		[Test]
-		[Category ("ItemKind Heal Type Test")]
-		public void ItemKindHealTypeTest() 
-		{
-			Assert.That (dIController.GetHealKind(), Is.TypeOf(typeof(DropItemController.ItemKind)));
-		}
+        [Test]
+        [Category("ItemKind Heal Type Test")]
+        public void ItemKindHealTypeTest()
+        {
+            Assert.That(dIController.GetHealKind(), Is.TypeOf(typeof(DropItemController.ItemKind)));
+        }
 
-		[Test]
-		[Category ("ItemKind itemKind Type Test")]
-		public void ItemKinditemKindTypeTest() 
-		{
-			Assert.That (dIController.itemKind(), Is.TypeOf(typeof(DropItemController.ItemKind)));
-		}
+        [Test]
+        [Category("ItemKind itemKind Type Test")]
+        public void ItemKinditemKindTypeTest()
+        {
+            Assert.That(dIController.itemKind(), Is.TypeOf(typeof(DropItemController.ItemKind)));
+        }
 
-		//Null値テスト(オブジェクト)
-		[Test]
-		[Category ("ItemKind Attack NotNull Test")]
-		public void NotNullAttackTest() 
-		{
-			Assert.NotNull (dIController.GetAttackKind());		
-		}
+        //Null値テスト(オブジェクト)
+        [Test]
+        [Category("ItemKind Attack NotNull Test")]
+        public void NotNullAttackTest()
+        {
+            Assert.NotNull(dIController.GetAttackKind());
+        }
 
-		[Test]
-		[Category ("ItemKind Heal NotNull Test")]
-		public void NotNullHealTest() 
-		{
-			Assert.NotNull (dIController.GetHealKind());		
-		}
+        [Test]
+        [Category("ItemKind Heal NotNull Test")]
+        public void NotNullHealTest()
+        {
+            Assert.NotNull(dIController.GetHealKind());
+        }
 
-		[Test]
-		[Category ("ItemKind itemKind NotNull Test")]
-		public void NotNullitemKindTest() 
-		{
-			Assert.NotNull (dIController.itemKind());		
-		}
-
-
-		//正常値テスト(メソッド)
-		[Test]
-		[Category ("IsPlayer Test")]
-		public void IsPlayerTest ()
-		{
-			string s = "Player";
-			Assert.True (dIController.IsPlayer(s));
-		}
-
-		[Test]
-		[Category ("IsNotPlayer Test")]
-		public void IsNotPlayerTest ()
-		{
-			string s = "Enemy";
-			Assert.False (dIController.IsPlayer(s));
-		}
-
-		[Test]
-		[Category ("IsTerrain Test")]
-		public void IsTerrainTest ()
-		{
-			string s = "Terrain";
-			Assert.True (dIController.IsTerrain(s));
-		}
-
-		[Test]
-		[Category ("IsNotTerrain Test")]
-		public void IsNotTerrainTest ()
-		{
-			string s = "Enemy";
-			Assert.False (dIController.IsTerrain(s));
-		}
-
-		[Test]
-		[Category ("CheckTerrain Test")]
-		public void CheckTerrainTest ()
-		{
-			string s = "Terrain";
-			bool flg = false;
-			dIController.CheckTerrain (s, flg);
-			Assert.False (iDrop.SetTrigger(true));
-
-		}
-
-		[Test]
-		[Category ("CanNot CheckTerrain Test")]
-		public void CanNotCheckTerrainTest ()
-		{
-			string s = "Player";
-			bool flg = false;
-
-			Assert.False (dIController.CheckTerrain (s, flg));
-		}
+        [Test]
+        [Category("ItemKind itemKind NotNull Test")]
+        public void NotNullitemKindTest()
+        {
+            Assert.NotNull(dIController.itemKind());
+        }
 
 
-		private IDropItemController GetItemMock () {
-			return Substitute.For<IDropItemController> ();
-		}
-		
-		private DropItemController GetControllerMock(IDropItemController iDrop) {
-			var dIController = Substitute.For<DropItemController> ();
-			dIController.SetDropItemController (iDrop);
-			return dIController;
-		}
-		
-	}
+        //正常値テスト(メソッド)
+        [Test]
+        [Category("IsPlayer Test")]
+        public void IsPlayerTest()
+        {
+            string s = "Player";
+            Assert.True(dIController.IsPlayer(s));
+        }
+
+        [Test]
+        [Category("IsNotPlayer Test")]
+        public void IsNotPlayerTest()
+        {
+            string s = "Enemy";
+            Assert.False(dIController.IsPlayer(s));
+        }
+
+        [Test]
+        [Category("IsTerrain Test")]
+        public void IsTerrainTest()
+        {
+            string s = "Terrain";
+            Assert.True(dIController.IsTerrain(s));
+        }
+
+        [Test]
+        [Category("IsNotTerrain Test")]
+        public void IsNotTerrainTest()
+        {
+            string s = "Enemy";
+            Assert.False(dIController.IsTerrain(s));
+        }
+
+        [Test]
+        [Category("CheckTerrain Test")]
+        public void CheckTerrainTest()
+        {
+            string s = "Terrain";
+            bool flg = false;
+            Assert.True(dIController.CheckTerrain(s, flg));
+        }
+
+        [Test]
+        [Category("CanNot CheckTerrain Test")]
+        public void CanNotCheckTerrainTest()
+        {
+            string s = "Player";
+            bool flg = false;
+
+            Assert.False(dIController.CheckTerrain(s, flg));
+        }
+
+
+        private IDropItemController GetItemMock()
+        {
+            return Substitute.For<IDropItemController>();
+        }
+
+        private DropItemController GetControllerMock(IDropItemController iDrop)
+        {
+            var dIController = Substitute.For<DropItemController>();
+            dIController.SetDropItemController(iDrop);
+            return dIController;
+        }
+
+    }
 }
 
